ab2f3bbc3ad690ef5ecc8224a573013f3cdde871
= <=>
2019-09-30 12:32:20 -0700

    Added a parse_arguments function and attempted to reimplement character input

--- main.cpp
+++ main.cpp
@@ -16,18 +16,23 @@ using namespace std;
 
 void draw_centered(WINDOW* win, int max_y, int max_x, string text);
 
-int main(void)
+void parse_arguments(int arg_count, char* args[]);
+
+int main(int argc, char* argv[])
 {
 	WINDOW* main_window = nullptr;
 	int num_cols = 0;
 	int num_rows = 0;
+	int row = 0;
+	int col = 0;
+	string filename = "test.txt";
 
+	//parse_arguments(argc,argv);
 	//SETUP
 	//initialize our window
 	main_window = initscr();
 
 	//resize our window
-	resize_term(5000, 5000);
 	getmaxyx(main_window, num_rows, num_cols);
 	resize_term(num_rows - 1, num_cols - 1);
 	getmaxyx(main_window, num_rows, num_cols);
@@ -53,29 +58,39 @@ int main(void)
 		wclear(main_window);
 
 		ostringstream temp_str{};
-		temp_str << "width: " << num_cols << " height: " << num_rows;
-		draw_centered(main_window, num_rows, num_cols, temp_str.str().c_str());
+		temp_str << "TX Text Editor: " << filename;
+		draw_centered(main_window, 0, num_cols, temp_str.str().c_str());
 		refresh();
 		int input = wgetch(main_window);
 
 		//Curses documentation says to use KEY_RESIZE, but you can also use
 		//is_termresized.  In real life, use either/or but not both.
+		/*
 		if (is_termresized() == true)
 		{
 			resize_term(0, 0);
 			getmaxyx(main_window, num_rows, num_cols);
 		}
+		*/
 		switch (input)
 		{
-		case ctrl('c'):
-			keep_going = false;
-		case KEY_RESIZE:
-			resize_term(0, 0);
-			getmaxyx(main_window, num_rows, num_cols);
+			case ctrl('c'):
+				keep_going = false;
+			case KEY_RESIZE:
+				resize_term(0, 0);
+				getmaxyx(main_window, num_rows, num_cols);
+			default:
+				mvwaddch(main_window,row,col,input);
 		}
 	}
 	//end curses mode
 	endwin();
+}
+
+void parse_arguments(int arg_count, char* args[]) {
+	for(int i=0; i<arg_count; i++) {
+
+	}
 }
 
 void draw_centered(WINDOW* win, int max_y, int max_x, string text)
@@ -85,3 +100,4 @@ void draw_centered(WINDOW* win, int max_y, int max_x, string text)
 	int top_margin = (max_y - 1) / 2;
 	mvwprintw(win, top_margin, left_margin, text.c_str());
 }
+
